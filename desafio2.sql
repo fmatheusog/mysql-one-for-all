fdf3ac92c37b27b3ae55111a46be49fdbb6a6b7a
Felipe Matheus <fmatheusog@gmail.com>
2022-03-16 09:53:59 -0300

    fix: req 1 & req 2: completo

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
 SELECT
-(SELECT count(*) from SpotifyClone.songs) AS musicas,
+(SELECT count(*) from SpotifyClone.songs) AS cancoes,
 (SELECT count(*) from SpotifyClone.artists) AS artistas,
 (SELECT count(*) from SpotifyClone.albums) AS albuns
